6aedb9392ce81627f195656260b3a9a6601f402d
Efrei-Paul <paul.cabon@efrei.net>
2021-09-20 14:20:56 +0200

    Comment

--- main.ps1
+++ main.ps1
@@ -1,22 +1,27 @@
-##Audit##
+#<Audit>
 
-#Affichage des informations générale sur la station : matériel, Bios, version OS, …
+<#
+Affichage des informations générale sur la station : matériel, Bios, version OS, …
+#>
 
+<#
+Affichage des informations sur les comptes locaux (privilèges attribués à chaque
+utilisateur, date de la dernière connexion, …etc) et vérification des paramètres des comptes.
+#>
 
-#Affichage des informations sur les comptes locaux (privilèges attribués à chaque
-#utilisateur, date de la dernière connexion, …etc) et vérification des paramètres des comptes.
+<#
+Afficher les paramètres de la vie privée, par exemple : Wifi sense (si version
+Windowsinférieure à 1709), SmartScreen Filter dans Edge, suggestions du menu de
+démarrage,Feedback, publicités (Advertising ID), localisation, Cortana, mise à jour windows peer-
+to peer (Windows Update P2P), traçage (Diagnostics Tracking Service), accès camera
+et microphone, …etc.
+#>
 
+<#
+Affichage des services à arrêter en application du principe de minimisation.
+#>
 
-#Afficher les paramètres de la vie privée, par exemple : Wifi sense (si version
-#Windowsinférieure à 1709), SmartScreen Filter dans Edge, suggestions du menu de
-#démarrage,Feedback, publicités (Advertising ID), localisation, Cortana, mise à jour windows peer-
-#to peer (Windows Update P2P), traçage (Diagnostics Tracking Service), accès camera
-#et microphone, …etc.
-
-
-#Affichage des services à arrêter en application du principe de minimisation.
-
-
-##Durcissement##
-
-#Durcissement de la couche réseau, protocole TLS et paramètres de cryptographie.
+<#Durcissement#>
+<#
+Durcissement de la couche réseau, protocole TLS et paramètres de cryptographie.
+#>
