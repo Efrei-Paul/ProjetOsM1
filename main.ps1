101f4688da9b14a9a1a52f37e973f6b6adbae97d
Efrei-Paul <paul.cabon@efrei.net>
2021-10-13 15:46:17 +0200

    password poicies

--- main.ps1
+++ main.ps1
@@ -66,7 +66,6 @@ Durcissement de la couche réseau, protocole TLS et paramètres de cryptographie
 
 
 ################### ENABELING TLS ###################
-#source https://docs.microsoft.com/en-us/mem/configmgr/core/plan-design/security/enable-tls-1-2-client
 
 Write-Host '#####################################'
 Write-Host '########## Disabling SSLv2 ##########'
@@ -123,9 +122,47 @@ New-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders
 Write-Host '[ + ] Done'
 
 
-################### HARDENING Crypto setings ###################
+################### HARDENING Crypto settings ###################
+
+#Max age of password: 30 days
+net accounts /maxpwage:30
+
+#Min age of password: 1 days
+net accounts /minpwage:1
+
+#Minimum length of password: 8 char
+net accounts /minpwlen:6
 
 
+#Managing Permissions
+md c:\Transcripts
+
+## Kill all inherited permissions 
+$acl = Get-Acl c:\Transcripts 
+$acl.SetAccessRuleProtection($true, $false)
+
+## Grant Administrators full control 
+$administrators = [System.Security.Principal.NTAccount] “Administrators” 
+$permission = $administrators,“FullControl”,“ObjectInherit,ContainerInherit”,“None”,“Allow” 
+$accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule $permission 
+$acl.AddAccessRule($accessRule)
+
+## Grant everyone else Write and ReadAttributes. This prevents users from listing ## transcripts from other machines on the domain. 
+$everyone = [System.Security.Principal.NTAccount] “Everyone” 
+$permission = $everyone,“Write,ReadAttributes”,“ObjectInherit,ContainerInherit”,“None”,“Allow” 
+$accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule $permission 
+$acl.AddAccessRule($accessRule)
+
+## Deny “Creator Owner” everything. This prevents users from ## viewing the content of previously written files. 
+$creatorOwner = [System.Security.Principal.NTAccount] “Creator Owner”
+$permission = $creatorOwner,“FullControl”,“ObjectInherit,ContainerInherit”,“InheritOnly”,“Deny” 
+$accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule 
+$permission $acl.AddAccessRule($accessRule)
+
+## Set the ACL 
+$acl | Set-Acl c:\Transcripts\
+
+## Create the SMB Share, granting Everyone the right to read and write files. Specific ## actions will actually be enforced by the ACL on the file folder. New-SmbShare -Name Transcripts -Path c:\Transcripts -ChangeAccess Everyone
 
 <#HTML#>
 $bar = "<hr class='separator separator--line' />"
